177573d6282987b1e0c02abb6a1fc388a7f235a7
Aleksandr Kadun <67789706+CoPHuK@users.noreply.github.com>
2021-01-22 16:17:37 +0200

    add Counrty List

--- src/js/coutriesList.js
+++ src/js/coutriesList.js
@@ -1,23 +1,36 @@
 import notify from './notif'
-import templates from '../tamlates/templates.hbs'
+import templates from '../tamplates/templates.hbs'
+import templatesList from '../tamplates/coutriesList.hbs'
 const listRef = document.querySelector('.infoBlock')
 
 function counriesList(countries) {
-
+console.log(countries.length);
     if (countries.length > 9) {
         notify.showNotificationAlert();
         return
     }
-    countries.map((country) => {
-        makeMarkUp(country)
+    else if (countries.length > 1 && countries.length < 10) {
+       
+        countries.map((country) => {
+            makeMarkUpList(country)
+            return
+        })
+    }
+    else {
+        makeMarkUp(countries[0])
         return
-    })
+    }
 }
   
-      
+
 function makeMarkUp(country) {
+    console.log(country);
     const markUp = templates(country);
     listRef.insertAdjacentHTML('beforeend', markUp)
+}
+    
+function makeMarkUpList(country) {
+    const markUp = templatesList(country);
+    listRef.insertAdjacentHTML('beforeend', markUp)
     }
-  
 export default counriesList
